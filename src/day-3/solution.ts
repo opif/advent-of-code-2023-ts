55e372ff566a688fe82810170851e324b3db6a52
Artur Raczyński <artur.raczynski@gmail.com>
2023-12-04 22:59:07 +0100

    day-3 moved to TS

--- src/day-3/solution.ts
+++ src/day-3/solution.ts
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import * as fs from "node:fs";
 
 const fileName = "input";
 const lines = fs
@@ -6,37 +6,37 @@ const lines = fs
   .split("\n")
   .filter((line) => !!line.trim());
 
-const partNumbers = new Map();
+const partNumbers = new Map<number, number>();
 let gearRatioSum = 0;
-const isSymbol = (char) => !/^[\.\d]$/.test(char);
-const isDigit = (char) => /^\d$/.test(char);
-const getPartNumber = (line, position) => {
+const isSymbol = (char: string) => !/^[\.\d]$/.test(char);
+const isDigit = (char: string) => /^\d$/.test(char);
+
+const getPartNumber = (line: string, position: number) => {
   while (position > 0 && isDigit(line[--position])) {}
   if (!isDigit(line[position])) {
     position++;
   }
 
-  return Number(line.slice(position).match(/^(\d+)/)[1]);
+  return Number(line.slice(position).match(/^(\d+)/)?.[1]);
 };
 
 lines.forEach((line, lineNo) => {
   [...line].forEach((c, charNo) => {
     if (isSymbol(c)) {
-      const parts = new Set();
+      const parts = new Set<number>();
       console.log(c, lineNo, charNo);
 
       for (
-        i = Math.max(lineNo - 1, 0);
+        let i = Math.max(lineNo - 1, 0);
         i <= lines.length - 1 && i <= lineNo + 1;
         i++
       ) {
         for (
-          j = Math.max(charNo - 1, 0);
+          let j = Math.max(charNo - 1, 0);
           j <= lines[lineNo].length - 1 && j <= charNo + 1;
           j++
         ) {
           if (isDigit(lines[i][j])) {
-            // console.log(getPartNumber(lines[i], j), i, j);
             parts.add(getPartNumber(lines[i], j));
           }
         }
@@ -62,6 +62,6 @@ const partSum = Array.from(partNumbers.entries()).reduce(
   (sum, [partNumber, count]) => sum + partNumber * count,
   0
 );
-// console.log(partNumbers);
+
 console.log(partSum);
 console.log(gearRatioSum);
